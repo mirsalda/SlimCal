15bc68894e0b21df4a1efe95a43247f964224eed
mirsalda <mirsaldad>
2025-01-14 23:32:42 +0100

    Ndryshim ne strukturen e folderave

---
+++ SlimCal/src/main/java/com/gr5B/SlimCal/SlimCalApplication.java
@@ -0,0 +1,13 @@
+package com.gr5B.SlimCal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlimCalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlimCalApplication.class, args);
+	}
+
+}
---
+++ SlimCal/src/test/java/com/gr5B/SlimCal/SlimCalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gr5B.SlimCal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlimCalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
