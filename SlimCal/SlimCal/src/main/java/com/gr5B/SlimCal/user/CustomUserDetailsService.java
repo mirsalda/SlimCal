d57ed953626ffcac8662b782b6aa31f1bfa7ab13
geraldl04 <geraldi2004@outlook.com>
2025-01-18 14:34:38 +0100

    Shtimi i SecurityConfig ,CustomUSerDetails dhe CustomUserDetaolsService per permiresimin a autentikimit te perdoruesve

---
+++ SlimCal/SlimCal/src/main/java/com/gr5B/SlimCal/user/CustomUserDetailsService.java
@@ -0,0 +1,36 @@
+package com.gr5B.SlimCal.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+
+        return new CustomUserDetails(
+                user.getId(),
+                user.getEmail(),
+                user.getPassword(),
+                user.getRole(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + user.getRole().name()))
+        );
+    }
+}
